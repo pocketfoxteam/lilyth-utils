212b0d909c75d55330c790c8a3b5c3fbc52e4e95
rurigk <rurigk@gmail.com>
2017-01-29 22:16:08 -0600

    Changes :0

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 var fs = require('fs')
 var path = require('path')
 
-var utils = {};
-var base = path.dirname(module.parent.filename);
-
-utils.saveData = function(n,d){
-	n = base+"/gendata/"+n;
-	if(fs.existsSync(base+"/gendata/")){
+function utils(base){
+	this.base = base;
+}
+utils.prototype.saveData = function(n,d){
+	
+	n = this.base+"/gendata/"+n;
+	if(fs.existsSync(this.base+"/gendata/")){
 		fs.writeFileSync(n, JSON.stringify(d), 'utf8');
 	}else{
-		fs.mkdirSync(base+"/gendata");
+		fs.mkdirSync(this.base+"/gendata");
 		fs.writeFile(n, JSON.stringify(d), 'utf8');
 	}
 }
-utils.loadData = function(n){
-	n = base+"/gendata/"+n;
+utils.prototype.loadData = function(n){
+	n = this.base+"/gendata/"+n;
 	if(fs.existsSync(n)){
 		return JSON.parse(fs.readFileSync(n,{encoding:'utf8'}));
 	}else{
 		return false;
 	}
 }
-
-module.exports = utils;
+function spawner(w){
+	return new utils(w);
+}
+module.exports = spawner;
